58125dd858c2f612dc534e4c4880ad7d1af74ce3
Galina <baleva1990@gmail.com>
2024-04-25 14:51:33 +0200

    NLV Theme: Making hero section dynamically

--- wp-content/themes/nlv/header.php
+++ wp-content/themes/nlv/header.php
@@ -21,63 +21,79 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Harbor<span>lights</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="oi oi-menu"></span> Menu
-            </button>
+            <a class="navbar-brand" href="<?php home_url() ?>">NLV <span>Apartments</span></a>
+            <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu">Menu</span>
+            </button> -->
 
             <div class="collapse navbar-collapse" id="ftco-nav">
-            <?php
-            wp_nav_menu($args = array(
-                'menu'                => "navbar-collapse", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
-                'menu_class'        => "navbar-nav ml-auto", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
-                'menu_id'            => "navbar-nav-id", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
-                'container'            => "ul", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
-                'container_class'    => "collapse navbar-collapse", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
-                'container_id'        => "ftco-nav", // (string) The ID that is applied to the container.
-                'theme_location'    => "headmenu", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
-            ));
-            ?>
-                <ul class="navbar-nav ml-auto" id="navbar-nav-id">
-                    <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="rooms.html" class="nav-link">Our Rooms</a></li>
-                    <li class="nav-item"><a href="restaurant.html" class="nav-link">Restaurant</a></li>
-                    <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
-                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                </ul>
+                <?php
+                wp_nav_menu($args = array(
+                    'menu'                => "navbar-collapse", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+                    'menu_class'        => "navbar-nav ml-auto", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+                    'menu_id'            => "navbar-nav-id", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
+                    'container'            => "ul", // (string) Whether to wrap the ul, and what to wrap it with. Default 'div'.
+                    'container_class'    => "collapse navbar-collapse", // (string) Class that is applied to the container. Default 'menu-{menu slug}-container'.
+                    'container_id'        => "ftco-nav", // (string) The ID that is applied to the container.
+                    'theme_location'    => "headmenu", // (string) Theme location to be used. Must be registered with register_nav_menu() in order to be selectable by the user.
+                ));
+                ?>
             </div>
         </div>
     </nav>
-    <!-- END nav -->
-    <div class="hero">
-        <section class="home-slider owl-carousel">
-            <div class="slider-item" style="background-image:url(<?php echo URL_IMG; ?>bg_1.jpg);">
-                <div class="overlay"></div>
-                <div class="container">
-                    <div class="row no-gutters slider-text align-items-center justify-content-end">
-                        <div class="col-md-6 ftco-animate">
-                            <div class="text">
-                                <h2>More than a hotel... an experience</h2>
-                                <h1 class="mb-3">Hotel for the whole family, all year round.</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="slider-item" style="background-image:url(<?php echo URL_IMG; ?>bg_2.jpg);">
-                <div class="overlay"></div>
-                <div class="container">
-                    <div class="row no-gutters slider-text align-items-center justify-content-end">
-                        <div class="col-md-6 ftco-animate">
-                            <div class="text">
-                                <h2>Harbor Lights Hotel &amp; Resort</h2>
-                                <h1 class="mb-3">It feels like staying in your own home.</h1>
+    <?php
+    if (is_front_page()) {
+    ?>
+        <!-- END nav -->
+        <div class="hero">
+            <section class="home-slider owl-carousel">
+                <div class="slider-item" style="background-image:url(<?php echo URL_IMG; ?>bg_2.jpg);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row no-gutters slider-text align-items-center justify-content-end">
+                            <div class="col-md-6 ftco-animate">
+                                <div class="text">
+                                    <h2>New Line Village Apartmentshotel</h2>
+                                    <h1 class="mb-3">It feels like staying in your own home.</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="slider-item" style="background-image:url(<?php echo URL_IMG; ?>bg_1.jpg);">
+                    <div class="overlay"></div>
+                    <div class="container">
+                        <div class="row no-gutters slider-text align-items-center justify-content-end">
+                            <div class="col-md-6 ftco-animate">
+                                <div class="text">
+                                    <h2>More than a holiday place... an experience</h2>
+                                    <h1 class="mb-3">Best place for the whole family.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    <?php
+    } else {
+    ?>
+        <!-- END nav -->
+        <div class="hero-wrap" style="background-image: url('<?php echo URL_IMG; ?>bg_3.jpg');">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text d-flex align-itemd-center justify-content-center">
+                    <div class="col-md-9 ftco-animate text-center d-flex align-items-end justify-content-center">
+                        <div class="text">
+                            <p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Home</a></span> <span>About Us</span></p>
+                            <h1 class="mb-4 bread"><?php the_title() ?></h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
-    </div>
+        </div>
+    <?php
+    };
+
+    ?>
